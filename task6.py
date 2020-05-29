387a3fc6e8d32abb03cc58d442a06c4310c096a1
bettapo <bettapo@yandex-team.ru>
2020-05-30 01:34:42 +0300

    Подправила 6ую лабу

--- task6.py
+++ task6.py
@@ -7,8 +7,8 @@ import modules.signs as s
 
 
 def get_normalized_image(pix):
-    img_grayscale = binary.apply_threshold(semitone.semitone(pix), 128, 1)
-    return u.pix_invert(img_grayscale, 0)
+    img_binary = binary.apply_threshold(semitone.semitone(pix), 128, 1)
+    return u.pix_invert(img_binary, 0)
 
 
 def load_model(path):
@@ -26,7 +26,6 @@ def load_model(path):
                 "y_axis": float(row[10]),
             }
 
-
     return result
 
 
@@ -40,22 +39,16 @@ def save_hypothesis(hyp, path):
             analysis_writer.writerow(res)
 
 
-
 def save_string(hypothesis, model_string, path):
     real = list(map(lambda d: max(d.items(), key=lambda x: x[1])[0], hypothesis))
     real_str = ''.join([str(elem) for elem in real])
     real_fixed = list(real_str.replace("ЬI", "Ы"))
     theory = list(model_string)
 
-   # correct = []
-    #for i in range(0, len(real_fixed)):
-       # correct.append(1 if real_fixed[i] == theory[i] else 0)
-
     with open(path, mode='w') as diff_string:
         diff_writer = csv.writer(diff_string, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         diff_writer.writerow(real_fixed)
         diff_writer.writerow(theory)
-        #diff_writer.writerow(correct)
 
 
 SAMPLES_PATH = "media/samples/text/"
@@ -66,12 +59,11 @@ SAMPLE_FONT = "fonts/arial_italic.ttf"
 
 tg.create_image_sample_text(SAMPLE_TEXT, SAMPLE_FONT, 52).save(SAMPLES_PATH + "/sample_52.bmp")
 tg.create_image_sample_text(SAMPLE_TEXT, SAMPLE_FONT, 40).save(SAMPLES_PATH + "/sample_40.bmp")
-tg.create_image_sample_text(SAMPLE_TEXT, SAMPLE_FONT, 60).save(SAMPLES_PATH + "/sample_70.bmp")
+tg.create_image_sample_text(SAMPLE_TEXT, SAMPLE_FONT, 60).save(SAMPLES_PATH + "/sample_60.bmp")
 
-# 1. Подготовить текст из одной строки, пользуясь выбранным алфавитом и теми же параметрами шрифта
 img_52 = get_normalized_image(u.picture_to_array(SAMPLES_PATH + "sample_52.bmp"))
 img_40 = get_normalized_image(u.picture_to_array(SAMPLES_PATH + "sample_40.bmp"))
-img_70 = get_normalized_image(u.picture_to_array(SAMPLES_PATH + "sample_70.bmp"))
+img_70 = get_normalized_image(u.picture_to_array(SAMPLES_PATH + "sample_60.bmp"))
 
 model_dict = load_model('dictionary/sign_table.csv')
 
@@ -81,8 +73,8 @@ hypothesis_70 = s.get_pix_hypothesis(img_70, model_dict)
 
 save_hypothesis(hypothesis_52, RESULTS_PATH + '/size_52/hypothesis.csv')
 save_hypothesis(hypothesis_40, RESULTS_PATH + '/size_40/hypothesis.csv')
-save_hypothesis(hypothesis_70, RESULTS_PATH + '/size_70/hypothesis.csv')
+save_hypothesis(hypothesis_70, RESULTS_PATH + '/size_60/hypothesis.csv')
 
 save_string(hypothesis_52, SAMPLE_TEXT_NO_SPACE, RESULTS_PATH + '/size_52/diff.csv')
 save_string(hypothesis_40, SAMPLE_TEXT_NO_SPACE, RESULTS_PATH + '/size_40/diff.csv')
-save_string(hypothesis_70, SAMPLE_TEXT_NO_SPACE, RESULTS_PATH + '/size_70/diff.csv')
+save_string(hypothesis_70, SAMPLE_TEXT_NO_SPACE, RESULTS_PATH + '/size_60/diff.csv')
